5d16b39b78763fb24b7b27d73c321fad6481dce4
Anwesh43 <anweshthecool0@gmail.com>
2019-02-01 08:31:00 +0530

    adding XToLadder class

--- xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
+++ xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
@@ -170,4 +170,28 @@ class XToLadderView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class XToLadder(var i : Int) {
+
+        private val root : XTLNode = XTLNode(0)
+        private var curr : XTLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
