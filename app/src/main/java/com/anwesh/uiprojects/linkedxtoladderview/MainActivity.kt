8b2bdc4246778fd521997d73dba6df524067a6e0
Anwesh43 <anweshthecool0@gmail.com>
2019-02-01 08:39:05 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedxtoladderview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedxtoladderview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedxtoladderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.xtoladderview.XToLadderView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : XToLadderView = XToLadderView.create(this)
     }
 }
