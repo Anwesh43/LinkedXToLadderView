773412b2532a93286de2fe086a516a5980bcba63
Anwesh43 <anweshthecool0@gmail.com>
2019-02-01 08:40:01 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedxtoladderview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedxtoladderview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedxtoladderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.xtoladderview.XToLadderView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view : XToLadderView = XToLadderView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
