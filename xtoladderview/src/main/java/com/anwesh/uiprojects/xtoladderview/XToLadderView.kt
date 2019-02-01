e26517bcfdf4f86d728fda5aa40dfe422c3ec544
Anwesh43 <anweshthecool0@gmail.com>
2019-02-01 08:23:46 +0530

    adding Animator class

--- xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
+++ xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
@@ -98,4 +98,32 @@ class XToLadderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
