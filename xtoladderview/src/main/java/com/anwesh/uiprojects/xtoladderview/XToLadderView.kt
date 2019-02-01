646770f8ad8d69e173a6e86b7597c5809c3b8295
Anwesh43 <anweshthecool0@gmail.com>
2019-02-01 07:31:13 +0530

    addin xtoladderview module

--- xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
+++ xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
@@ -21,3 +21,10 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.8f
 val foreColor : Int = Color.parseColor("#01579B")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap  
