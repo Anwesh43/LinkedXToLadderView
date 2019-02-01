81358ab2995550069c7f070c49b3dad49967e986
Anwesh43 <anweshthecool0@gmail.com>
2019-02-01 08:02:52 +0530

    adding XToLadderView class

--- xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
+++ xtoladderview/src/main/java/com/anwesh/uiprojects/xtoladderview/XToLadderView.kt
@@ -61,3 +61,21 @@ fun Canvas.drawXTLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class XToLadderView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas: Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
